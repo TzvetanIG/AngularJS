b192ce556939a58be6a231fc1f6aea4439dc7eea
TzvetanIG <cecoig@mail.bg>
2014-12-17 12:22:52 +0200

    Add First Homework Introduction-to-AngularJS

---
+++ Introduction-to-AngularJS/js/controllers.js
@@ -0,0 +1,26 @@
+'use strict';
+var app = app || {};
+
+app.controller('StudentPage', function ($scope) {
+    var student = {
+        name: "Pesho",
+        photo: "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+        grade: 5,
+        school: "High School of Mathematics",
+        teacher: "Gichka Pesheva"
+    };
+
+    $scope.student = student;
+});
+
+app.controller('BindImageSourceToTextBox', function ($scope) {
+    var student = {
+        name: "Pesho",
+        photo: "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+        grade: 5,
+        school: "High School of Mathematics",
+        teacher: "Gichka Pesheva"
+    };
+
+    $scope.student = student;
+});
