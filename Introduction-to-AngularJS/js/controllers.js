765fd24b66de8e89be8a126cad47a53f6a1c3a52
TzvetanIG <cecoig@mail.bg>
2014-12-17 12:29:28 +0200

    fix bug

--- Introduction-to-AngularJS/js/controllers.js
+++ Introduction-to-AngularJS/js/controllers.js
@@ -13,14 +13,6 @@ app.controller('StudentPage', function ($scope) {
     $scope.student = student;
 });
 
-app.controller('BindImageSourceToTextBox', function ($scope) {
-    var student = {
-        name: "Pesho",
-        photo: "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
-        grade: 5,
-        school: "High School of Mathematics",
-        teacher: "Gichka Pesheva"
-    };
+app.controller('BindImageSourceToTextBox', function () {
 
-    $scope.student = student;
 });
